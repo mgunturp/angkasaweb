7fe1042246febf4f666c05e580b50b3d93a6b628
Guntur Pratama <mgunturp123@gmail.com>
2024-01-22 13:06:12 +0700

    add key

--- src/app/components/cardslider.js
+++ src/app/components/cardslider.js
@@ -57,8 +57,8 @@ function Slider() {
       <p className="text-[#2395FF] font-[500] text-[14px] tracking-[.25em] mb-4">TRENDING</p>
       <h1 className="text-[24px] font-[500] mb-20">Trending destinations</h1>
       <Swiper modules={[Pagination, Autoplay, EffectCoverflow]} slidesPerView={5} spaceBetween={20} autoplay={{ delay: 1500 }} pagination={{ el: ".swiper-pagination", clickable: true }} className="swiper_container  w-full">
-        {ImageSlider.map((item) => (
-          <SwiperSlide className="px-2">
+        {ImageSlider.map((item, index) => (
+          <SwiperSlide key={index}  className="px-2">
             <div className="relative h-[262px] overflow-hidden rounded-lg w-[206px]">
               <Image src={item.image} alt="slide_image" layout="fill" objectFit="cover" className="rounded-lg transform hover:scale-105 transition-transform duration-300" />
               <div className="airline absolute bg-white bg-opacity-[40%] top-5 left-3 py-[4px] px-4 rounded-full">
